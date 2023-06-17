43d1e6b1f25f683c7cc86dc4fdd3a6b4878755dd
Ana Guernelli <anabeatrizmgp@gmail.com>
2023-06-17 12:13:43 -0300

    Closure

--- section4/functions/class74.py
+++ section4/functions/class74.py
@@ -4,4 +4,13 @@ Closure
 '''
 
 
-def make_salute(salute, name):
+def make_greeting(greeting):
+    def greet(name):
+        return f'{greeting}, {name}!'
+    return greet
+
+good_morning_greet = make_greeting('Good morning')
+good_night_greet = make_greeting('Good night')
+
+for nome in ['Ana', 'Banana', 'Pedro']:
+    print(good_morning_greet(nome))
