4d183914da5a2faf9a9c492ef868a1373e646b3c
anaguernelli <anabeatrizmgp@gmail.com>
2023-10-24 14:37:38 -0300

    Modules - import, from, alias

---
+++ section4/class96.py
@@ -0,0 +1,13 @@
+'''
+    !! Não é uma boa prática importar tudo (*) de um módulo
+       ou substituir as variáveis desses módulos
+
+    ! Você pode dar apelidos/alias a objetos
+
+'''
+
+from sys import platform as pl
+
+
+# print(platform)
+print(pl)
